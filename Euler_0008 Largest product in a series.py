261abebe4dc284a06cce111b721fa7da273defb3
ronniegane <ronniegane@gmail.com>
2015-09-09 11:41:02 +1200

    Update and rename adjacent digits.py to Euler_0008 Largest product in a series.py
    Formatting to match other Euler problems

--- Euler_0008 Largest product in a series.py
+++ Euler_0008 Largest product in a series.py
@@ -1,3 +1,7 @@
+''' Project Euler problem 8: largest product in a series
+Find the thirteen adjacent digits in the 1000-digit number 
+that have the greatest product. What is the value of this product?'''
+
 bigNumber = """73167176531330624919225119674426574742355349194934
 96983520312774506326239578318016984801869478851843
 85861560789112949495459501737958331952853208805511
