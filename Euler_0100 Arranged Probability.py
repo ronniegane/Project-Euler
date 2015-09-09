a6d3cc039b0bbb301f15ad496bc1de6921d4398e
Ronnie Gane <ronniegane@gmail.com>
2015-09-09 23:43:39 +1200

    Added naive approach code. Good for boxes up to T=1m. Much slower after that.

--- Euler_0100 Arranged Probability.py
+++ Euler_0100 Arranged Probability.py
@@ -13,4 +13,30 @@ By finding the first arrangement to contain
 over 10**12 = 1,000,000,000,000 discs in total,
 determine the number of blue discs that the box would contain.'''
 
-# want 
+# want B/T * (B-1)/(T-1) = 1/2 where B and T are whole numbers
+# so T(T-1) = 2B(B-1)
+# or in quadratic form with B constant:
+# T**2 - T -2B(B-1) = 0
+# Quadratic
+# Loop up through numbers of B: B has to be odd
+from math import sqrt, ceil, floor
+T = 1
+B = 1
+while T < 10000000:
+    coeff = 1 + 8 * (B**2 - B)
+    # print(coeff)
+    if (sqrt(coeff) % 2) - 1 < 0.00000001:
+        #valid B and T
+        T = (1 + sqrt(1+8*(B**2 - B)))/2
+        if T % 1 <0.00000001:
+            T = int(round(T,0))
+            print("%s blue discs in %s total (%s red)" % (B, T, T-B))
+    # increment
+    B+=1
+
+# TO DO:
+# -Code is pretty slow for T > 10 m.
+# -Different approach: treat as a solving algorithm like newton's method or
+# whatever equivalent for 3D problems (as we are trying to solve a 3D quadratic)
+# Could even just use binary approach for now
+# Compare T and B and adjust to get fraction closer to a goal.
