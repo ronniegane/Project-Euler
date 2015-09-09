228aee827570830e335e219fc15486442e2989df
Ronnie Gane <ronniegane@gmail.com>
2015-09-09 12:00:10 +1200

    Adding header

--- primefactors.py
+++ primefactors.py
@@ -1,14 +1,20 @@
+'''Project Euler problem 3: Largest prime factor
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?'''
+
 # finding prime factors
-# Trial division maybe?
+
 
 from math import sqrt, floor
 myNum = 600851475143
 halfway = int(floor(sqrt(myNum)))
+
 print(sqrt(600851475143))
 factorsOfNum = []
-for possible in range(2, halfway):
+for possible in range(2, halfway): # possible factors of myNum
     if myNum % possible == 0:
-        # prime factor
+        #this is a factor, prime or otherwise
         factorsOfNum.append(possible)
 
 maxFactor = 0
