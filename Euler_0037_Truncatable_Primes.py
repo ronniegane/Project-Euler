61b9acdecd692cadd4fa687a5e75594b1692618c
Ronnie Gane <ronniegane@gmail.com>
2015-09-17 15:05:45 +1200

    Completed problem 37, gets correct answer.

--- Euler_0037_Truncatable_Primes.py
+++ Euler_0037_Truncatable_Primes.py
@@ -11,4 +11,35 @@ NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 '''
 
 # Use isPrime code from previous works
-import Euler_0027_Quadratic_Primes
+from Euler_0027_Quadratic_Primes import isPrime
+
+
+# Start with a list of primes up to 1 million
+primeList = [str(x) for x in range(1000000) if isPrime(x)]
+
+#print(primeList[:20])
+
+# work through the primeList
+
+leftTrunc = []
+rightTrunc = []
+bothTrunc = []
+
+for prime in primeList:
+    for index in range(1,len(prime)):
+        left = prime[:-index]
+        right = prime[index:]
+        if (not isPrime(int(left))) or (not isPrime(int(right))):
+            break
+    else:
+        bothTrunc.append(prime)
+
+print(bothTrunc)
+
+# sum up the last 11 of these
+mySum = 0
+for truncPrime in bothTrunc[-11:]:
+    print(truncPrime)
+    mySum += int(truncPrime)
+
+print("Total sum is %s" % mySum)
