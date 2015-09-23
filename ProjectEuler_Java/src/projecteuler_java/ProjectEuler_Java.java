f4dffe40952286857099c579ccc18ca1c26eb39c
Ronnie Gane <ronniegane@gmail.com>
2015-09-23 19:51:32 +1200

    Created unit tests for Problem 3. Added primality tester. isPrime() function passing all tests.

--- ProjectEuler_Java/src/projecteuler_java/ProjectEuler_Java.java
+++ ProjectEuler_Java/src/projecteuler_java/ProjectEuler_Java.java
@@ -5,6 +5,9 @@
  */
 package projecteuler_java;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.floor;
+
 /**
  *
  * @author Ronnie
@@ -15,7 +18,43 @@ public class ProjectEuler_Java {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /**
+         * Project Euler problem 3: Largest prime factor
+         * The prime factors of 13195 are 5, 7, 13 and 29.
+         * What is the largest prime factor of the number 600851475143 ?
+         */
+        System.out.println("Hello World");
+        double myNum; // needs to be a double as it is a very large number
+        System.out.println(6.0 % 4);
+        
+        myNum = 600851475143.0;
+        largestPrimeFactor(myNum); // Find largest prime
+                
     }
+    
+    public static boolean isPrime(double x){
+        //number must be round and greater than 1
+        
+        
+        if ((floor(x) < 2)|(abs(x % 1) > 0.00001)){
+            return false;
+        }
+                
+        // Checks primality by simple trial division
+        for(int i = 2; i < java.lang.Math.sqrt(x); i++){
+            if (floor(x) % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static double largestPrimeFactor(double x){
+        System.out.println(x);
+        // Work out all factors
+        // Check if a factor is prime
+        return 1.0;
+    }
+    
     
 }
