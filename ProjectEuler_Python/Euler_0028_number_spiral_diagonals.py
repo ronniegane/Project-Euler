d119e9af72d60abacae81e4e7dba3cd16dbf7d9f
Ronnie Gane <ronniegane@gmail.com>
2015-11-07 17:52:08 +1300

    Problem 28 completed correctly.

--- ProjectEuler_Python/Euler_0028_number_spiral_diagonals.py
+++ ProjectEuler_Python/Euler_0028_number_spiral_diagonals.py
@@ -13,3 +13,23 @@ It can be verified that the sum of the numbers on the diagonals is 101.
 What is the sum of the numbers on the diagonals in a 1001 by 1001
 spiral formed in the same way?
 '''
+
+# the pattern seems to be: start at 1, add 2, 2, 2, 2, then 4, 4, 4, 4, then 6, 6, 6, 6,...
+# and square grows 1x1 -> 3x3 -> 5x5... odd numbers with the square as the last number
+
+size = 1001 # side length of the square
+
+count = 1 # start at 1
+currentNum = 1
+addNum = 2 # At first, add two to the count for each corner
+
+for currentSize in range(3,size+1, 2):
+    for i in range(4):
+        currentNum += addNum
+        count += currentNum
+    addNum += 2
+
+print(count)
+
+
+
